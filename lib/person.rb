ad7dbd2b3505688810cb71be5c786605fa7a46f7
Chandler Craig <hellochandlercraig@gmail.com>
2020-11-26 16:29:19 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+
+  def name=(person_name)
+    @this_persons_name = person_name
+  end
+
+  def name
+    @this_persons_name
+  end
+
+  def breed=(dog_breed)
+    @this_dogs_breed = dog_breed
+  end
+
+  def breed
+    @this_dogs_breed
+  end
+
+end
